28c33395a70878a0f5db62477b00836a3449c202
niico <5653050+niicojs@users.noreply.github.com>
2023-11-11 15:07:40 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,117 @@
+import { useState, useCallback, useEffect } from 'react';
+import { useDropzone } from 'react-dropzone';
+import removeBackground from '@imgly/background-removal';
+import { DownloadCloud } from 'lucide-react';
+
+import { Button } from './components/ui/button';
+import { cn } from './lib/utils';
+
+type Img = File & { preview: string; working?: boolean; processed?: string };
+
+function App() {
+  const [files, setFiles] = useState<Img[]>([]);
+  const [processing, setProcessing] = useState<string | undefined>(undefined);
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // heic & heic2any
+    // preload from background-removal ?
+    setFiles(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+          working: false,
+        })
+      )
+    );
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: { 'image/*': [] },
+  });
+  useEffect(() => {
+    return () =>
+      files.forEach((file) => {
+        URL.revokeObjectURL(file.preview);
+        if (file.processed) {
+          URL.revokeObjectURL(file.processed);
+        }
+      });
+  }, []);
+
+  const doTheThing = async () => {
+    if (files) {
+      for (const file of files) {
+        setProcessing(file.name);
+        const buffer = await file.arrayBuffer();
+        const blob = await removeBackground(buffer, {
+          output: {
+            quality: 0.8,
+            type: 'foreground',
+            format: 'image/png',
+          },
+        });
+        file.processed = URL.createObjectURL(blob);
+        setProcessing(undefined);
+      }
+    }
+  };
+
+  return (
+    <section className="container text-center mt-10">
+      <h1 className="font-bold text-xl uppercase">Background Remover</h1>
+      <div
+        className="mx-10 mt-10 p-5 border bg-slate-100 h-48 flex items-center text-slate-400"
+        {...getRootProps()}
+      >
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p className="w-full text-balance">Drop the files here ...</p>
+        ) : (
+          <p className="w-full text-balance">
+            Drag 'n' drop some files here, or click to select files
+          </p>
+        )}
+      </div>
+      <div>
+        <Button
+          className="my-10 uppercase"
+          onClick={() => doTheThing()}
+          disabled={!!processing}
+        >
+          Do The Thing!
+        </Button>
+      </div>
+      <div className="flex flex-wrap gap-2 justify-center">
+        {files.map((img) => (
+          <div
+            key={img.name}
+            className="p-2 border border-2 border-solid border-black relative"
+          >
+            <img
+              className="object-scale-down w-48 h-48"
+              src={img.processed || img.preview}
+            />
+            <div
+              className={cn(
+                'absolute w-full h-full left-0 top-0 backdrop-filter backdrop-blur flex items-center justify-center white-shadow',
+                { hidden: img.name !== processing }
+              )}
+            >
+              processing...
+            </div>
+            <div
+              className={cn('absolute right-2 bottom-2', {
+                hidden: !img.processed,
+              })}
+            >
+              <a href={img.processed} download={`nobg-${img.name}`}>
+                <DownloadCloud className="w-5 h-5" />
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default App;
